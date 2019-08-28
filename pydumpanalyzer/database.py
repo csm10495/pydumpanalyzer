123a908ef92383c2a3ca0bbddc0eb745161ad88a
Charles Machalow <csm10495@gmail.com>
2019-08-27 17:58:20 -0700

    Add more filled gitignore, add vscode settings, move out database tests

--- pydumpanalyzer/database.py
+++ pydumpanalyzer/database.py
@@ -3,7 +3,6 @@ import csmlog
 import os
 import sqlite3
 import threading
-import unittest
 
 from server import ROOT_STORAGE_LOCATION
 
@@ -129,77 +128,3 @@ class Database(object):
                                                                                    keys=','.join(list(rowAsDict.keys())),
                                                                                    values=','.join(map(lambda x: '"' + str(x) + '"', list(rowAsDict.values()))))
         return self.booleanExecute(sqlStatement)
-
-class TestDatabase(unittest.TestCase):
-    ''' quick unit tests for the database class '''
-
-    def setUp(self):
-        ''' called at the start of each test case '''
-        logger.info("starting test case: %s" % self.id())
-        self.database = Database(':memory:')
-        self.database.open()
-
-    def tearDown(self):
-        ''' called at the end of each test case '''
-        self.database.close()
-        logger.info("ending test case: %s" % self.id())
-
-    def test_create_table(self):
-        ''' ensure we can create tables '''
-        assert not self.database.tableExists('MyTable')
-
-        assert self.database.createTable('MyTable', [
-            Column("ColumnName", "TEXT")
-        ])
-
-        assert not self.database.createTable('MyTable', [
-            Column("ColumnName", "TEXT")
-        ])
-
-        assert self.database.tableExists('MyTable')
-        assert not self.database.tableExists('MyTable2')
-
-    def test_add_row(self):
-        ''' ensure we can add rows to a given table '''
-        assert not self.database.addRow('MyTable', {
-            "ColumnName" : "ColumnData"
-        })
-
-        assert self.database.createTable('MyTable', [
-            Column("ColumnName", "TEXT")
-        ])
-
-        assert self.database.addRow('MyTable', {
-            "ColumnName" : "ColumnData"
-        })
-
-        assert self.database.execute('SELECT * FROM MYTABLE').fetchall()[0].ColumnName == 'ColumnData'
-
-    def test_ensure_has_columns(self):
-        ''' ensure we can add columns if needed '''
-
-        assert self.database.createTable('MyTable', [
-            Column("TextColumn1", "TEXT")
-        ])
-
-        assert self.database.ensureTableHasAtLeastTheseColumns('MyTable', [
-            Column('TextColumn1', "TEXT"),
-            Column('TextColumn2', "INT"),
-            Column('TextColumn3', "TEXT"),
-        ])
-
-        assert self.database.addRow('MyTable', {
-            'TextColumn1' : 'Hello',
-            'TextColumn2' : 123,
-        })
-
-        lastInsert = self.database.execute('SELECT * from MYTABLE').fetchall()[-1]
-        assert lastInsert.TextColumn2 == 123
-        assert lastInsert.TextColumn1 == 'Hello'
-
-        assert not self.database.ensureTableHasAtLeastTheseColumns('MyTable', [
-            Column('TextColumn1', "TEXT"),
-            Column('TextColumn2', "INT"),
-            Column('TextColumn3', "INT"), # wrong type!
-        ])
-
